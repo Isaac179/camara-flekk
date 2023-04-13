833048b6884ba622c3587a977a5f1d04aaffe33e
isaac179 <fablabtoluca@gmail.com>
2023-04-13 14:33:25 -0600

    ajuste cambio de camara gpt 4

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+
 
 
 </head>
@@ -54,13 +54,15 @@
     </p>
   </footer>
 
-  <script src="./app.js"></script>
+  <!--<script src="./app.js"></script>-->
   
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.js"></script>
 
 <script type="text/javascript">
 
+
     function configure() {
+      
         Webcam.set({
             width: 480,
             height: 360,
