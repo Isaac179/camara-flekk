3b90fc2ff21c71aea4d3eb63e5d49c1be3cc66b2
isaac179 <fablabtoluca@gmail.com>
2023-04-18 13:06:25 -0600

    zbarjs

--- index.php
+++ index.php
@@ -1,101 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.min.css" rel="stylesheet" />
 
-    <title>Document</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Captura evidencias autopartes</title>
+  <link rel="stylesheet" href="./app.css" />
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
-<body>
-<div class="row">
-  <div class="columns">
-   <h1> Scanner APP Test (Quagga JS)
-     <div id="interactive" class="viewport"></div>
-      <div id="result_strip">
-        <ul class="thumbnails"></ul>
-        <ul class="collector"></ul>
-      </div>
-        <div class="controls">
-                <button class="stop">Stop</button>
-            <fieldset class="reader-config-group">
-               <label style="display: none">
-                    <span>Torch</span>
-                    <input type="checkbox" name="settings_torch" />
-                </label>
-                <label>
-                    <span>Barcode-Type</span>
-                    <select name="decoder_readers">
-                        <option value="code_128" >Code 128</option>
-                        <option value="code_39">Code 39</option>
-                        <option value="code_39_vin">Code 39 VIN</option>
-                        <option selected="selected" value="ean">EAN</option>
-                        <option value="ean_extended">EAN-extended</option>
-                        <option value="ean_8">EAN-8</option>
-                        <option value="upc">UPC</option>
-                        <option value="upc_e">UPC-E</option>
-                        <option value="codabar">Codabar</option>
-                        <option value="i2of5">Interleaved 2 of 5</option>
-                        <option value="2of5">Standard 2 of 5</option>
-                        <option value="code_93">Code 93</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Resolution (width)</span>
-                    <select name="input-stream_constraints">
-                        <option value="320x240">320px</option>
-                        <option selected="selected" value="640x480">640px</option>
-                        <option value="800x600">800px</option>
-                        <option value="1280x720">1280px</option>
-                        <option value="1600x960">1600px</option>
-                        <option value="1920x1080">1920px</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Patch-Size</span>
-                    <select name="locator_patch-size">
-                        <option value="x-small">x-small</option>
-                        <option value="small">small</option>
-                        <option selected="selected" value="medium">medium</option>
-                        <option value="large">large</option>
-                        <option value="x-large">x-large</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Half-Sample</span>
-                    <input type="checkbox" checked="checked" name="locator_half-sample" />
-                </label>
-                <label>
-                    <span>Workers</span>
-                    <select name="numOfWorkers">
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option selected="selected" value="4">4</option>
-                        <option value="8">8</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Camera</span>
-                    <select name="input-stream_constraints" id="deviceSelection">
-                    </select>
-                </label>
-                <label style="display: none">
-                    <span>Zoom</span>
-                    <select name="settings_zoom"></select>
-                </label>
-               
-            </fieldset>
+
+<body onload="configure();">
+
+  <header>
+    <h1>Registro de refacciónes</h1>
+  </header>
+
+  <main>
+
+<br><br>
+        
+    <div style="align-items: center; display: flex;flex-direction: column;">
+        <div id="my_camera">
         </div>
-     
-      
-    </section>
-  </div>
-  
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation/foundation.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <div id="results" style="visibility: hidden; position: absolute;">
+            
+        </div>
+    </div>
+    <br>
+ 
+    <div class="container" style="text-align: center;">
+      <button type="button SUBMIT" onclick="saveSnap();">Tomar foto</button>
+      <a href="image.php"><button type="button" name="button">Ver base de imagenes&#x2192;</button> </a>  </div>
+    <div><br></div>
+
+  </main>
+  <footer>
+    
+    <p>flekk camara
+      <a href="mailto:icalderon@flekk.com">@webmaster</a>
+    </p>
+  </footer>
+ 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.js"></script>
+
+<script type="text/javascript">
+function configure() {
+    Webcam.set({
+        width: 480,
+        height: 360,
+        image_format: 'jpeg',
+        jpeg_quality: 90,
+        constraints: {
+            facingMode: 'environment'
+        }
+    });
+
+    Webcam.attach('#my_camera');
+}
+
+function saveSnap() {
+    Webcam.snap(function(data_uri){
+        document.getElementById('results').innerHTML = 
+            '<img id="webcam" src="'+data_uri+'">';
+    });
+
+    Webcam.reset();
+
+    var base64image = document.getElementById("webcam").src;
+    Webcam.upload(base64image,'function.php',function(code,text){
+        alert('Imagen guardada con exito');
+        document.location.href = "image.php"
+    });
+
+}
+
+</script>
+
+
 </body>
+
 </html>
