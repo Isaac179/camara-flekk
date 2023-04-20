e9613f6a1d7b8973dce9b1550c8c35c84746a301
isaac179 <fablabtoluca@gmail.com>
2023-04-20 11:41:11 -0600

    se agrego campo para referencia de pedido

--- main.js
+++ main.js
@@ -86,3 +86,35 @@ function renderData(ctx, data, x, y) {
           ctx.clearRect(0, 0, stream_canvas.width, stream_canvas.height)
         },100)
     }
+
+    //scripts camara evidencias
+
+    function configure() {
+      Webcam.set({
+          width: 480,
+          height: 360,
+          image_format: 'jpeg',
+          jpeg_quality: 90,
+          constraints: {
+              facingMode: 'environment'
+          }
+      });
+  
+      Webcam.attach('#my_camera');
+  }
+  
+  function saveSnap() {
+      Webcam.snap(function(data_uri){
+          document.getElementById('results').innerHTML = 
+              '<img id="webcam" src="'+data_uri+'">';
+      });
+  
+      Webcam.reset();
+  
+      var base64image = document.getElementById("webcam").src;
+      Webcam.upload(base64image,'function.php',function(code,text){
+          alert('Imagen guardada con exito');
+          document.location.href = "image.php"
+      });
+  
+  }
